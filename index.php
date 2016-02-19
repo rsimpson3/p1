a480a04d4f73e3268af1e59a7dc25001e3e2b732
Robert Simpson <robert_simpson@hms.harvard.edu>
2016-02-19 16:59:11 -0500

    Replace index.html with index.php, edit nested ul tags, add doc type declarations associated with Creative Commons license embed W3 validation errors.

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>';?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+                      "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html version="XHTML+RDFa 1.0" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>Robert Simpson Dynamic Web Applications Portfolio</title>
+	<link href="css/index.css" rel="stylesheet">
+</head>
+<body>
+<!-- Google Analytics tracking test -->
+<?php include_once("analyticstracking.php") ?>
+	<div class="wrapper">
+		<div class="portrait">
+			<h1>Robert Simpson</h1>
+			<img src="images/simpson_headshot_300px.jpg" alt="simpson headshot">
+			<h2> Dynamic Web Applications Portfolio</h2>
+		</div>
+		<ol class="projects">
+			<li>Project 1</li>
+				<ul>
+					<li>View [<a href="http://p1.digitalbiolab.me/">Link</a>] </li>
+					<li>Github [<a href="https://github.com/rsimpson3/p1">Link</a>] </li>
+				</ul>
+			<li>Project 2</li>
+				<ul>
+					<li>View [<a href="http://p2.digitalbiolab.me/">Link</a>] </li>
+					<li>Github [<a href="https://github.com/rsimpson3/p2">Link</a>] </li>
+				</ul>
+			<li>Project 3</li>
+				<ul>
+					<li>View [<a href="http://p3.digitalbiolab.me/">Link</a>] </li>
+					<li>Github [<a href="https://github.com/rsimpson3/p3">Link</a>] </li>
+				</ul>
+			<li>Project 4</li>
+				<ul>
+					<li>View [<a href="http://p4.digitalbiolab.me/">Link</a>] </li>
+					<li>Github [<a href="https://github.com/rsimpson3/p4">Link</a>] </li>
+				</ul>
+		</ol>
+		<section class="mybackgrd">
+			<h1> Who Am I? </h1>
+			<p>Hi, I'm Robert Simpson. I work at Harvard Medical School in the <a href="https://mfdp.med.harvard.edu/"> Office of Diversity Inclusion and Community Partnership </a> as a K-12 Science Curriculum Specialist. We design engaging STEM programs for Boston and Cambridge public school students. Our programs are part of Harvard's commitment to provide educational outreach opportunities to local students. </p>
+
+			<h2>Why Am I Taking This Course?</h2>
+			<p> I'm interested in how mobile devices can be used to expand learning opportunities for today's hyper-connected students. I manage the Hinton Scholars AP Biology program serving a hundred Boston High School students. One challenge students face while taking a rigorous science courses is how to reflect on their own learning and develop the skills to ask good questions. Based on my previous instructional design course work, I want to build and test mobile-friendly interactive modules that students can access on their mobile devices. The <a href="https://voice.adobe.com/a/B59lL/"> Digital BioLab Trailer</a> gives a good introduction. I'm new to the world of web applications programming. </p>
+
+			<h3> Previous Experience Programming </h3>
+			<p>I'm comfortable designing front-end web solutions with html editors such as Adobe Dreamweaver and hand-coding html using text editors such as Sublime Text 3. I've taken introductory courses in Java and Javascript through the Harvard Extension School. I completed the Adobe Generation Professional: App Design bootcamp in June 2015. I learned how Adobe Brackets, JQuery, CSS, and PhoneGap Build can be used to build and test mobile apps. I highly recommend the next <a href="https://edex.adobe.com/pd/course/apps-2016/"> free online course </a> running Jun 27, 2016 - Aug 12, 2016 on the Adobe Education Exchange.</p>
+
+			<h4> Computing Platform </h4>
+			<p>I use an Apple MacBook Pro laptop running OS 10.10.5. </p>
+
+
+			<h5> A Bit More </h5>
+			<p>I'm a Harvard Extension School ALM candidate studying Digital Media Design. I'm also an Adobe Education Leader. </p>
+		</section>
+		<section class="socialmedia">
+			<h1> Social Media </h1>
+			    <a class="twitter-timeline"  href="https://twitter.com/neotech03" data-widget-id="481801531633577984">Tweets by @neotech03</a>
+	            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+		</section>
+		<footer id="footer">
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Web Applications Portfolio</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://www.linkedin.com/in/rsimpson3" property="cc:attributionName" rel="cc:attributionURL">Robert Simpson</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="http://p1.digitalbiolab.me/" rel="dct:source">http://p1.digitalbiolab.me/</a>. | Harvard Medical School | Office for Diversity Inclusion and Community Partnership
+		</footer>
+	</div>
+
+</body>
+</html>
