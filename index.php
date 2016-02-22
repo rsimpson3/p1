0a3a5eb4c424215702c672699b508c76ae7c1b0f
Robert Simpson <robert_simpson@hms.harvard.edu>
2016-02-22 00:12:21 -0500

    fix creative commons tag.

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 
 		</section>
 		<footer id="footer">
-            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a> <This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. | Harvard Medical School | Office for Diversity Inclusion and Community Partnership
+            <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a> This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. | Harvard Medical School | Office for Diversity Inclusion and Community Partnership
 		</footer>
 	</div>
 
